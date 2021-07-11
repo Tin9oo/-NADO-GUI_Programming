032ebdf4f820ffd783d8de8212374113c317e078
Tin9oo <rlawnghks99@gmail.com>
2021-07-11 17:00:23 +0900

    ADD frame

--- gui_basic/1_create_frame.py
+++ gui_basic/1_create_frame.py
@@ -3,6 +3,6 @@ from tkinter import *
 root =Tk()
 root.title("My GUI")    # Title
 root.geometry("640x480+100+300")    # Size & Position
-                                    # width * length * x pos * y pos
+                                    # width * length + x pos + y pos
 
 root.mainloop()
