fef1f0334ae86ee4f8a4584b0727c78b23f878aa
Tin9oo <rlawnghks99@gmail.com>
2021-07-11 17:45:19 +0900

    ADD Button

--- gui_basic/1_create_frame.py
+++ gui_basic/1_create_frame.py
@@ -2,7 +2,9 @@ from tkinter import *
 
 root =Tk()
 root.title("My GUI")    # Title
-root.geometry("640x480+100+300")    # Size & Position
-                                    # width * length + x pos + y pos
+root.geometry("640x480")    # Size & Position
+                            # width * length + x pos + y pos
+
+root.resizable(False, False)    # x(width), y(length) value cannot be changed (window size cannot be changed)
 
 root.mainloop()
