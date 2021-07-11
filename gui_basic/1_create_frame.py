3b4618e81a0bda4ea2f50afd0bcc2499817c84d4
Tin9oo <rlawnghks99@gmail.com>
2021-07-11 16:54:21 +0900

    CREATE repo

---
+++ gui_basic/1_create_frame.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+root =Tk()
+root.title("My GUI")    # Title
+root.geometry("640x480+100+300")    # Size & Position
+                                    # width * length * x pos * y pos
+
+root.mainloop()
